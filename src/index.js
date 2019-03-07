38ce1b5720b4e7f6675e28358da07a4061ffd81e
NikKireev <bill.sypher.95@bk.ru>
2019-03-07 10:38:43 +0300

    monye-exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency < 0) return {};
+    if (currency > 10000) return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+
+    var H = Math.floor(currency / 50);
+    var Q = Math.floor((currency - H * 50) / 25);
+    var D = Math.floor((currency - H * 50 - Q * 25) / 10);
+    var N = Math.floor((currency - H * 50 - Q * 25 - D * 10) / 5);
+    var P = Math.floor((currency - H * 50 - Q * 25 - D * 10 - N * 5) / 1);
+
+    var arr = { H,Q,D,N,P };
+
+    Object.keys(arr).forEach((key) => {
+        if (arr[key] === 0) delete arr[key];
+    })
+    return arr;
 }
